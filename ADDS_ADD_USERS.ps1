9aa94cbc512a40472fc4e816e07b40c645dc4b96
BlacKnife123 <piotrwn106@gmail.com>
2022-11-10 11:31:40 +0100

    Add Users

---
+++ ADDS_ADD_USERS.ps1
@@ -0,0 +1,34 @@
+﻿#Add users frmo CSV file
+$Path = "C:\Users\Administrator\Documents\Book1.csv"
+$ADUsers = Import-Csv "$Path"
+
+foreach ($User in $ADUsers)
+{
+    $Username   = $User.username
+    $Password   = $User.password
+    $Firstname  = $User.firstname
+    $Lastname   = $User.lastname
+    $Department = $User.department
+    $OU         = $User.ou
+
+    if (Get-ADUser -F {SamAccountName -eq $Username})
+    {
+    Write-Warning "User $Username has already exist"
+    }
+    else
+    {
+    New-ADUser `
+    -SamAccountName $Username `
+    -Name "$Firstname $Lastname" `
+    -GivenName $Firstname `
+    -Surname $Lastname `
+    -Enabled $true `
+    -ChangePasswordAtLogon $true `
+    -Department $Department `
+    -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
+    }
+}
+
+
+#Add single user to domain
+New-ADUser -Name "NAME" -ChangePasswordAtLogon $true -AccountPassword (Read-Host -AsSecureString "password") -Enabled $true
