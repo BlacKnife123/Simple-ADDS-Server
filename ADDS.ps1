241df581288fc06820a03b182afa01359051e11a
BlacKnife123 <piotrwn106@gmail.com>
2022-11-10 11:31:24 +0100

    ADDS-Server

---
+++ ADDS.ps1
@@ -0,0 +1,18 @@
+﻿$NICNAME = "LAN"
+$IFINDEX = (Get-NetAdapter | Where-Object Name -EQ "$NICNAME" | Select-Object -ExpandProperty "IfIndex")
+$IPADDRESS = "10.40.40.1"
+$PREFIX = "24"
+$MASK = "255.255.255.0"
+$LOOPBACK = "127.0.0.1"
+$SERVICES = "AD-Domain-Services"
+$DOMAINNAME = "Name.local"
+$BIOSNAME = "NAME"
+$PASSWORD = (ConvertTo-SecureString -String "PASSWORD" -AsPlainText -Force)
+
+New-NetIPAddress -InterfaceIndex "$IFINDEX" -IPAddress "$IPADDRESS" -PrefixLength "$PREFIX"
+Set-DnsClientServerAddress -InterfaceIndex "$IFINDEX" -ServerAddresses "$LOOPBACK"
+
+Install-WindowsFeature -Name "$SERVICES" -IncludeManagementTools
+
+Install-ADDSForest -DomainName "$DOMAINNAME" -DomainNetBiosName "$BIOSNAME" -InstallDns -SafeModeAdministratorPassword $PASSWORD
+
